04cb96a302d643b2d941025108ae211b2873ef2a
Echzio <aleksassincreed@yandex.ru>
2022-03-15 20:38:28 +0300

    [fix] name styles

--- src/features/name/view.tsx
+++ src/features/name/view.tsx
@@ -5,14 +5,14 @@ const Name: React.FC<{ spotify: SpotifyWebApi.SpotifyWebApiJs }> = ({ spotify })
   const me = useMe(spotify);
 
   return (
-    <div>
+    <div className="flex flex-col gap-y-4">
       {me.display_name && <h1 className="text-white font-bold text-3xl">Hello, {me.display_name}</h1>}
       {me?.external_urls?.spotify && (
         <a
           href={me.external_urls.spotify}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white text-sm underline hover:opacity-70 transition-opacity duration-150">
+          className="text-white text-sm underline hover:opacity-70 transition-opacity duration-150 w-max">
           Spotify profile link
         </a>
       )}
