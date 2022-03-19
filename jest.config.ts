8a0c6f18894663481e3c19d943ac47231c267101
Echzio <aleksassincreed@yandex.ru>
2022-03-19 20:15:01 +0300

    [add] testing-library

--- jest.config.ts
+++ jest.config.ts
@@ -125,7 +125,7 @@ const config: Config.InitialOptions = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['regenerator-runtime/runtime'],
+  setupFilesAfterEnv: ['regenerator-runtime/runtime', '<rootDir>/setupTests.js'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
