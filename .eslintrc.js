bbc50240d0d323596bb40cb50ddca8fa7b49f40b
Echzio <aleksassincreed@yandex.ru>
2022-03-23 22:53:29 +0300

    [fix] eslint stylelint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,23 +5,24 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:react/jsx-runtime",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "react/prop-types": 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/prop-types': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
   },
-};
+}
